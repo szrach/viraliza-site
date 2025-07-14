54c2d42b590c6845c3421cb75de7bed0803f48d8
szrach <seb.zap@outlook.com>
2025-07-14 20:03:37 +0200

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div style={{ backgroundColor: '#000', color: '#fff', minHeight: '100vh', padding: '4rem' }}>
+      <h1 style={{ fontSize: '3rem' }}>VIRALIZA</h1>
+      <p>Built for creators who move markets.</p>
+    </div>
+  );
+}
